ba32d112d3ac3c6d17e8618ba3d5fa520320243d
J C <james@jamescampbell.us>
2016-06-08 14:09:15 -0400

    Create dbconfig.py

---
+++ dbconfig.py
@@ -0,0 +1,11 @@
+#/usr/local/bin/python
+'''
+Author: James Campbell
+Date Created: June 8th 2016
+Date Updated:
+What: Username and Password info for database connect in log2mysql.py
+Requirements: MySQL and Python. pip3 install mysqlclient or pip install MySQL-Python if 2.x
+'''
+
+userconfig = 'your user here'
+userpass = 'your pass here'
